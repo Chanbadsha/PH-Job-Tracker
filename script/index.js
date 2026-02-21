7432dabcad504eefcfe00ac2a84e33ccfdab7952
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-02-21 23:19:27 +0600

    Create job card and implement interview and rejected btn behivior

--- script/index.js
+++ script/index.js
@@ -11,12 +11,82 @@ function allJobCount() {
 
   jobCount.innerText = allJobs.children.length;
   totalJobNumberShow.innerText = allJobs.children.length;
-  rejectedJob.innerText = rejectedJob.length;
+  rejectedJobNumberShow.innerText = rejectedJob.length;
   interviewJobNumberShow.innerText = interViewJob.length;
 }
 allJobCount();
 
 function interViewJobClick() {
   const jobData = document.getElementById("interview-btn");
-  console.log(jobData);
 }
+
+const allJobs = document.getElementById("allJobs");
+
+allJobs.addEventListener("click", function (event) {
+  const parentNode = event.target.parentNode.parentNode;
+
+  const companyName = parentNode.querySelector(".companyName").innerText;
+  const position = parentNode.querySelector(".position").innerText;
+  const location = parentNode.querySelector(".location").innerText;
+  const type = parentNode.querySelector(".type").innerText;
+  const salary = parentNode.querySelector(".salary").innerText;
+  const description = parentNode.querySelector(".description").innerText;
+
+  const jobData = {
+    companyName,
+    location,
+    position,
+    type,
+    salary,
+    description,
+  };
+
+  if (event.target.classList.contains("interview")) {
+    let existJob;
+    for (let job of interViewJob) {
+      if (
+        job.companyName == jobData.companyName &&
+        job.position == jobData.position
+      ) {
+        existJob = true;
+        break;
+      }
+    }
+
+    if (!existJob) {
+      interViewJob.push(jobData);
+      rejectedJob = rejectedJob.filter(
+        (job) =>
+          !(
+            job.companyName == jobData.companyName &&
+            job.position == jobData.position
+          ),
+      );
+      allJobCount();
+    }
+  } else if (event.target.classList.contains("rejected")) {
+    let existJob;
+    for (let job of rejectedJob) {
+      if (
+        job.companyName == jobData.companyName &&
+        job.position == jobData.position
+      ) {
+        existJob = true;
+        break;
+      }
+    }
+
+    if (!existJob) {
+      rejectedJob.push(jobData);
+
+      interViewJob = interViewJob.filter(
+        (job) =>
+          !(
+            job.companyName == jobData.companyName &&
+            job.position == jobData.position
+          ),
+      );
+      allJobCount();
+    }
+  }
+});
